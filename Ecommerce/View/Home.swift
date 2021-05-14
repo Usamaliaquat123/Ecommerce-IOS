144b1f1127bac4a7d3291074d568199ce8a8af51
Usamaliaquat123 <inconnent12345@outlook.com>
2021-05-15 01:57:35 +0500

    add navigation in menu

---
+++ Ecommerce/View/Home.swift
@@ -0,0 +1,107 @@
+//
+//  Home.swift
+//  Ecommerce
+//
+//  Created by Mac on 15/05/2021.
+//
+
+import SwiftUI
+
+struct Home: View {
+    @State var selected = Tabs[0]
+    @Namespace var animation
+    var body: some View {
+        
+        
+        VStack{
+            HStack{
+                Button(action: {}){
+                    Image(systemName: "line.horizontal.3.decrease")
+                        .font(.system(size: 25, weight: .heavy))
+                        .foregroundColor(.black)
+                }
+                Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/){
+                    Image(systemName: "person.crop.circle")
+                        .resizable()
+                        .foregroundColor(.black)
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 30, height: 30)
+                        .cornerRadius(15)
+                    
+                }
+            }
+            .padding(.vertical, 10)
+            .padding(.horizontal)
+            
+            
+            Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+            
+            
+            ScrollView {
+                VStack{
+                    HStack{
+                        VStack(alignment: .leading, spacing: 5){
+                            Text("let's")
+                                .font(.title)
+                                .foregroundColor(.black)
+                            Text("Get Started")
+                                .font(.largeTitle)
+                                .fontWeight(.heavy)
+                                .foregroundColor(.black)
+                        }.padding(.horizontal)
+                        Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+                    }
+                
+//              Tabs
+                    HStack(spacing: 0){
+                        ForEach(Tabs, id: \.self){ tab in
+                            Tabbutton(title: tab, selected: $selected, animation: animation)
+                            
+                            // even spacing
+                            if Tabs.last != tab{
+                                Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+                            }
+                            
+                            
+                        }
+                    }.padding()
+                
+                }
+            }
+            
+            
+            
+            
+            
+            
+    
+            
+            
+        }.background(Color.white.ignoresSafeArea())
+    }
+}
+
+#if DEBUG
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
+#endif
+
+
+var Tabs = ["Glasses", "Watched", "Shoes", "Perfumes"]
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Ecommerce/View/Tabbutton.swift
@@ -0,0 +1,56 @@
+//
+//  Tabbutton.swift
+//  Ecommerce
+//
+//  Created by Mac on 15/05/2021.
+//
+
+import SwiftUI
+
+struct Tabbutton: View {
+    var title : String
+    @Binding var selected: String
+    var animation : Namespace.ID
+    var body: some View {
+       
+        Button(action: {
+            withAnimation(.spring()){
+                selected = title
+            }
+            
+        }){
+            Text(title)
+                .font(.system(size: 15))
+                .foregroundColor(selected == title ? .white : .black)
+                .padding(.vertical, 10)
+                .padding(.horizontal, selected == title ? 20 : 0)
+                .background(
+                
+                    ZStack(){
+                        if(selected == title){
+                            Color(.black)
+                                .clipShape(Capsule())
+                                .matchedGeometryEffect(id: "Tab", in: animation)
+                        }
+                        
+                  
+                    }
+                
+                )
+            }
+    }
+}
+
+
+
+
+
+//
+//#if DEBUG
+//struct Tabbutton_Previews: PreviewProvider {
+//    static var previews: some View {
+//        Tabbutton('sd',)
+//    }
+//}
+//
+//#endif
