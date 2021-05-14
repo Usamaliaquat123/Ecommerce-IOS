14acdfeda2c89251b006bad17f0d48596d5a84e6
Usamaliaquat123 <inconnent12345@outlook.com>
2021-05-15 01:05:23 +0500

    Initial Commit

---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by Mac on 15/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
